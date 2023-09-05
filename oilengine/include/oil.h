139e3be75e11046c59abb06adcce69d494052210
Wardronthewarden <matelajosdev@gmail.com>
2023-09-05 21:19:08 +0200

    late commit to upload missed cmake files

---
+++ oilengine/include/oil.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+// FOR USE BY APPLICATIONS
+
+#include "oilengine_export.h"
+
+#include "oil/Application.h"
+#include "oil/Log.h"
+
+//-------Entry point------------
+
+#include "oil/EntryPoint.h"
+
+//------------------------------
